4b4f750f41554844149292c62f938d91a4c7ea1a
Jesus Alejandro Hernandez Martinez <jesus.hernandezm@axity.com>
2024-04-17 23:32:20 -0600

    Update send.py

--- send.py
+++ send.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-import logging
-import pika
+"""Module providing a function printing python version."""
+
 import sys
 import time
+import logging
+import pika
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,22 +11,30 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 logging.info('Init Sender')
+
 logging.info('Sleep 3s')
 time.sleep(3)
-logging.info('Config credentials to RabbitMQ')
-credentials = pika.PlainCredentials('default_user_HyVrwYiHL_OO94aaM_X', 'nV0c9NekNaRY7SJ8R-q4Qr3rntx1FBW2')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='mide.default.svc.cluster.local', credentials=credentials))
-channel = connection.channel()
 
+logging.info('Config credentials to RabbitMQ')
+credentials = pika.PlainCredentials(
+    'default_user_HyVrwYiHL_OO94aaM_X', 'nV0c9NekNaRY7SJ8R-q4Qr3rntx1FBW2')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='mide.default.svc.cluster.local',
+        credentials=credentials))
+
+channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "La tuya!"
+MESSAGE = ' '.join(sys.argv[1:]) or "La tuya!"
+
 channel.basic_publish(
     exchange='',
     routing_key='task_queue',
-    body=message,
+    body=MESSAGE,
     properties=pika.BasicProperties(
         delivery_mode=pika.DeliveryMode.Persistent
     ))
-logging.info(f"[x] Sent {message}")
+
+logging.info("[x] Sent %s", MESSAGE)
 connection.close()
